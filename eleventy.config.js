39e488a6d4171ef20b4f4aa6820869b9d86806c4
Martin Berglund <martin.berglund@nav.no>
2025-02-05 11:09:01 +0100

    Update config

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,14 +2,47 @@ import { EleventyHtmlBasePlugin } from '@11ty/eleventy'
 import { eleventyImageTransformPlugin } from '@11ty/eleventy-img'
 import eleventyNavigationPlugin from '@11ty/eleventy-navigation'
 
+import * as sass from 'sass'
+import path from 'path'
+
 export default async function (eleventyConfig) {
   // Watch images for the image pipeline.
-  eleventyConfig.addWatchTarget('content/**/*.{svg,webp,png,jpg,jpeg,gif}')
+  eleventyConfig.addWatchTarget('*.scss')
+
+  // Passthrough Copy
+  eleventyConfig.addPassthroughCopy('assets/js')
+  eleventyConfig.addPassthroughCopy('assets/images/logo-small.svg')
+  eleventyConfig.addPassthroughCopy('assets/images/nav-logo.svg')
+  eleventyConfig.addPassthroughCopy('assets/images/mim-banner.png')
+
+  // Sass stuff
+  eleventyConfig.addTemplateFormats('scss')
+  eleventyConfig.addExtension('scss', {
+    outputFileExtension: 'css', // optional, default: "html"
+
+    // `compile` is called once per .scss file in the input directory
+    compile: async function (inputContent, inputPath) {
+      let parsed = path.parse(inputPath)
+      if (parsed.name.startsWith('_')) {
+        return
+      }
+
+      let result = sass.compileString(inputContent, {
+        loadPaths: [parsed.dir || '.', this.config.dir.includes],
+      })
+
+      this.addDependencies(inputPath, result.loadedUrls)
+
+      // This is the render function, `data` is the full data cascade
+      return async (data) => {
+        return result.css
+      }
+    },
+  })
 
   // Eleventy Plugins
-  eleventyConfig.addPlugin(EleventyHtmlBasePlugin)
-
   eleventyConfig.addPlugin(eleventyNavigationPlugin)
+  eleventyConfig.addPlugin(EleventyHtmlBasePlugin)
 
   eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
     formats: ['avif', 'webp', 'auto'],
@@ -26,18 +59,19 @@ export default async function (eleventyConfig) {
     },
   })
 
-  eleventyConfig.addBundle('css', {
-    toFileDirectory: '_site_new',
-  })
-
   // Filters
   eleventyConfig.addShortcode('currentBuildDate', () => {
     return new Date().toISOString()
   })
+
+  eleventyConfig.setLiquidOptions({
+    dynamicPartials: false,
+    root: ['_includes', '.'],
+  })
 }
 
 export const config = {
-  templateFormats: ['md', 'njk', 'html'],
+  templateFormats: ['md', 'html'],
   dir: {
     includes: '_includes',
     layouts: '_layouts',
