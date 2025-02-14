d6d6cd543b00fa26f2d4810012e5d14d70a9899d
Martin Berglund <martin.berglund@nav.no>
2025-02-14 23:46:07 +0100

    Add global date variable

--- _data/site.js
+++ _data/site.js
@@ -1,7 +1,10 @@
+import { DateTime } from 'luxon';
+
 export default {
   mode: process.env.ELEVENTY_RUN_MODE,
   title: 'Mangfold i mai',
   summary: 'Feir mangfold, inkludering og universell utforming!',
   // url: 'https://mangfoldimai.no',
   url: 'https://navikt.github.io',
+  today: DateTime.fromISO(new Date().toISOString())
 }
