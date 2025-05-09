dbd70dd5bf728560a6545f649727cffaad75ac4b
Martin Berglund <martin.berglund@nav.no>
2025-05-09 13:55:28 +0200

    Remove site edit link

--- src/_data/site.js
+++ src/_data/site.js
@@ -4,7 +4,8 @@ export default {
   mode: process.env.ELEVENTY_RUN_MODE,
   title: 'Mangfold i mai',
   summary: 'Feir mangfold, inkludering og universell utforming!',
-  // url: 'https://mangfoldimai.no',
-  url: 'https://navikt.github.io/mangfold-i-mai/',
+  url: 'https://mangfoldimai.no',
+  // url: 'https://navikt.github.io/mangfold-i-mai/',
+  github_repository: 'https://github.com/navikt/mangfold-i-mai/',
   today: DateTime.fromISO(new Date().toISOString()),
 }
