de49edad04c563eb25b872445ae780ed7fca3880
macbruker <macbruker@users.noreply.github.com>
2025-05-12 06:41:31 +0000

    deploy: b5c10df143ba1bd156cc1721a2a73248dd13752a

---
+++ 2023/assets/js/program.js
@@ -0,0 +1,37 @@
+function main() {
+	var radios = document.forms['program-filter-form'].elements['filter'];
+	for(var i = 0; i < radios.length; i++) {
+	    radios[i].onclick = function() {
+	        filterEvents(this.value);
+	    }
+	}
+
+}
+
+function filterEvents(value) {
+	var eventList = document.getElementById('event-list');
+	var events = eventList.getElementsByTagName('li');
+
+	for(var i = 0; i < events.length; i++) {
+
+		if (value === 'all') {
+
+			events[i].style.display = 'block';
+
+		} else {
+
+			var badges = events[i].getElementsByClassName('badge');
+	    	var match = false;
+
+	    	for (var j = 0; j < badges.length; j++) {
+	    		if (badges[j].className.indexOf(value) !== -1) match = true;
+	    	}
+
+	    	if (match) events[i].style.display = 'block';
+	    	if (!match) events[i].style.display = 'none';
+
+		}
+	}
+}
+
+main();
