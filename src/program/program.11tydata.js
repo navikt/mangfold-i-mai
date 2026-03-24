4407bdf548583d01ec3d3f97cf92ef715ca48635
Martin Berglund <martin.berglund@nav.no>
2026-03-24 15:27:29 +0100

    Unnta foredrag fra indeksering

--- src/program/program.11tydata.js
+++ src/program/program.11tydata.js
@@ -1,6 +1,7 @@
 export default {
   layout: 'event.html',
   tags: ['event'],
+  noindex: true,
   eleventyComputed: {
     day: (data) => data['event-date'].split('T')[0],
     time: (data) => data['event-date'].split('T')[1],
